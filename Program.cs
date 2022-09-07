efedc7d65abca8f4af88bc1a381cba5d33db3c13
subas <subash.neruturi@gmail.com>
2022-09-07 13:05:55 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Foreach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // for (int i = 0; i < 10; i++)
+            //{
+            //      Console.WriteLine("Value of i: {0}", i);
+
+            //Foreach
+
+            string[] carCompanies = { "Tata Motors", "Mahindra", "Volkswagen", "Toyota" };
+
+            foreach (string car in carCompanies)
+            {
+                Console.WriteLine("{0}", car);
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
